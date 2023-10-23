c4b2350b9580c358420ef8879d451d1b5227ba5d
Hany <hany@hanykhodair.com>
2023-10-23 13:14:39 +0300

    updated Routes

---
+++ lib/layout/home_layout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeLayout extends StatelessWidget {
+static const String routeName= 'homeLayout';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
