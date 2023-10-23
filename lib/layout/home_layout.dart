75f186661f60795c27e07b75616f7a4c2ec1ef49
Hany <hany@hanykhodair.com>
2023-10-23 19:47:49 +0300

    Home Layut

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,25 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/shard/network/remote/api_manager.dart';
+import 'package:movie_app/shard/style/colors.dart';
 
 class HomeLayout extends StatelessWidget {
-static const String routeName= 'homeLayout';
+  static const String routeName = 'homeLayout';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-extendBody: true,
-backgroundColor: Colors.black,
-bottomNavigationBar: BottomAppBar(
-color: Colors.black,
-child:
-  BottomNavigationBar(
+    <<<<<<< developer
+    appBar: AppBar(),
+    backgroundColor: black,
+    body: FutureBuilder(
+    future: ApiManager.getPopular(),
+    builder: (context, snapshot) {
+    if (snapshot.connectionState == ConnectionState.waiting) {
+    return const Center(
+    child: CircularProgressIndicator(
+    color: Colors.amber,
+    ));
+    }
+    if (snapshot.hasError) {
+    return const Center(
+    child: Text(
+    "error",
+    style: TextStyle(color: Colors.white),
+    ));
+    }
+
+    var movies = snapshot.data?.results ?? [];
+    return ListView.builder(
+    itemBuilder: (context, index) {
+    return Text(
+    movies[index].title ?? "",
+    style: const TextStyle(color: Colors.white),
+    );
+    },
+    itemCount: movies.length,
+    );
+    },
+    ),
+    =======
+    extendBody: true,
     backgroundColor: Colors.black,
-onTap: (value) {
+    bottomNavigationBar: BottomAppBar(
+    color: Colors.black,
+    child:
+    BottomNavigationBar(
+    backgroundColor: Colors.black,
+    onTap: (value) {
 
-},
-items: [
+    },
+    items: [
 
-],
-  ),
-),
+    ],
+    ),
+    ),
+    >>>>>>> master
     );
   }
 }
