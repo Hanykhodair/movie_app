d85c34adafef94b844f13f781278bc046ee403bd
Ahmed Hassen <ahsn50071@gmail.com>
2023-10-25 12:01:42 +0300

    delete test code in home layout

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:movie_app/shard/network/remote/api_manager.dart';
-import 'package:movie_app/shard/style/colors.dart';
 
 class HomeLayout extends StatelessWidget {
   static const String routeName = 'homeLayout';
@@ -8,36 +6,7 @@ class HomeLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      backgroundColor: black,
-      body: FutureBuilder(
-        future: ApiManager.getPopular(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(
-                child: Text(
-              "error",
-              style: TextStyle(color: Colors.white),
-            ));
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-                child: CircularProgressIndicator(
-              color: Colors.amber,
-            ));
-          }
-          var movies = snapshot.data?.results ?? [];
-          return ListView.builder(
-            itemBuilder: (context, index) {
-              return Text(
-                movies[index].title ?? "",
-                style: const TextStyle(color: Colors.white),
-              );
-            },
-            itemCount: movies.length,
-          );
-        },
-      ),
+
     );
   }
 }
