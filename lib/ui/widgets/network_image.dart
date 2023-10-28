d57c36d998f70375656e2a996bf06397b1f0b0ba
AbdelhadyMohamed <86628356+AbdelhadyMohamed@users.noreply.github.com>
2023-10-28 14:26:17 +0200

    bookmark

--- lib/ui/widgets/network_image.dart
+++ lib/ui/widgets/network_image.dart
@@ -14,14 +14,17 @@ class ImageNetwork extends StatelessWidget {
       children: [
         Image.network("https://image.tmdb.org/t/p/w500/$path",
             width: 129, height: 199),
-        Icon(Icons.bookmark,
-            color: isAddedToWatchList ? AppColors.yellowColor : Colors.grey,
-            size: 60),
-        Padding(
-          padding: const EdgeInsets.only(top: 10, left: 17),
-          child: Icon(isAddedToWatchList ? Icons.done : Icons.add,
-              color: Colors.white),
-        )
+        isAddedToWatchList
+            ? const Icon(
+                Icons.bookmark_added,
+                color: AppColors.yellowColor,
+                size: 60,
+              )
+            : const Icon(
+                Icons.bookmark_add_rounded,
+                color: Colors.grey,
+                size: 60,
+              ),
       ],
     );
   }
